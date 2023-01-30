6355a68581b5e2d6d43228c6544c1097b6618487
c2run <jose.cerda66@gmail.com>
2023-01-30 13:50:26 -0300

    saludo Función

--- src/componentes/SaludoFuncion.jsx
+++ src/componentes/SaludoFuncion.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState} from 'react';
 import PropTypes from 'prop-types';
 
 
 const SaludoFuncion = (props) => {
+    //Para trabajar con estados en funciones se utiliza useState
+    //const [variable, método para actualizarlo] = useState(valor inicial)
+    const [edad, setedad] = useState(30);
+
+    const cumple = () => {
+        //actualizamos el State
+        setedad(edad+1)
+    }
     return (
         <div>
                 <h1>
                     Hola!!! {props.nombre} desde componente funcional!
                 </h1>
-                
+                <h2>
+                    Tu edad es de: {edad}
+                </h2>
+                <div>
+                    <button onClick={cumple}>
+                        cumplir años
+                    </button>
+                </div>
                 
             </div>
     );
